5b59c2382d083a0f7b052b42ba40c8c497751e88
libassalaye24@gmail.com <libassalaye24@gmail.com>
2021-06-09 10:03:57 +0000

    first commit with quizz project

---
+++ config/constantes.php
@@ -0,0 +1,5 @@
+<?php 
+        define("ROUTE_DIR" , str_replace('public' ,'',$_SERVER['DOCUMENT_ROOT']));
+        define("WEB_ROUTE" , "http://localhost:8001/");
+
+?>
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php 
+require("../config/constantes.php");
+    require_once(ROUTE_DIR.'views/security/connexion.html.php');
+   
+?>
