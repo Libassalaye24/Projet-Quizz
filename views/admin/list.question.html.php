ad731d8c69578a749cd0d47d7e8c9815adf2547a
libassalaye24@gmail.com <libassalaye24@gmail.com>
2021-06-14 15:44:49 +0000

    menu

--- views/admin/list.question.html.php
+++ views/admin/list.question.html.php
@@ -1,17 +1,5 @@
 <?php 
    require_once(ROUTE_DIR.'views/imc/header.html.php');
- 
+   require_once(ROUTE_DIR.'views/imc/menu.html.php');
     require_once(ROUTE_DIR.'views/imc/footer.html.php');
 ?>
-<div class="container">
-    <div class="row">
-        <div class="row">
-             
-       
-        </div>
-       
-            <div class="col-8">
-                <?php   require_once(ROUTE_DIR.'views/imc/menu.html.php'); ?>
-            </div>
-    </div>
-</div>
