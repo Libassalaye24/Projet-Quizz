c59cf48fcc20cb6b1f94db244ab30b32fbfeb770
libassalaye24@gmail.com <libassalaye24@gmail.com>
2021-06-17 14:06:02 +0000

    constantes

--- config/constantes.php
+++ config/constantes.php
@@ -1,6 +1,6 @@
 <?php 
         define("WEB_ROUTE" , "http://localhost:8000/");
-        //define("WEB_ROUTE" , "http://libasse24.alwaysdata.net/");
+       // define("WEB_ROUTE" , "http://libasse24.alwaysdata.net/");
 
         define("ROUTE_DIR" , str_replace('public' ,'',$_SERVER['DOCUMENT_ROOT']));
 
